85fea5a2d925ddf283d5899d77cd3882d1a404cd
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2022-12-30 22:14:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ const Circle = require('./circle');
 
 
 
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
